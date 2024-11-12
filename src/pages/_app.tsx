aa3aaec09cfc4908f40afaa70b63d1ee0cf0dbb5
Carlos Romero <cmromero.dev@gmail.com>
2024-11-12 02:06:50 -0600

    fix: Update import path for FirebaseProvider in _app.tsx to resolve error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import { FirebaseProvider } from '../../context/FirebaseContext';
+import { FirebaseProvider } from '../context/FirebaseContext';
 import '../styles/globals.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
